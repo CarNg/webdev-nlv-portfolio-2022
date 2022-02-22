7ab7ee1e88958ae4f17a3ab9a3c53893f9eb3602
Car <CarNg@users.noreply.github.com>
2022-02-22 20:38:17 +0800

    Setup Project Sections - Responsive

--- src/components/ProjectsGrid/ProjectsGrid.js
+++ src/components/ProjectsGrid/ProjectsGrid.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './projects-grid.css';
+import MediaQuery from 'react-responsive';
 
 export default class projectsGrid extends Component {
     render() {
         const projects = this.props.projects.map((project) => {
             return (
                 <React.Fragment key={project.id} >
-                    <div className="projects-grid-project-thumbnail" style={project.grid}></div>
+                    <MediaQuery minWidth={768}>
+                        <div className="projects-grid-project-thumbnail" style={project.grid}></div>
+                    </MediaQuery>
+                    <MediaQuery maxWidth={767}>
+                        <div className="projects-grid-project-thumbnail"></div>
+                    </MediaQuery>
                 </React.Fragment>
             )
         })
