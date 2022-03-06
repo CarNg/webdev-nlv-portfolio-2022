0abe8aa04411a54b1ed29c84d16302cc1cf996e5
Car <CarNg@users.noreply.github.com>
2022-03-06 20:10:45 +0800

    Add License Key For FullPage.js

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
+import { LicenseKey } from '../assets/files/license'
 
 import Header from '../components/Header/Header'
 import Welcome from '../components/Welcome/Welcome'
 import About from '../components/About/About'
 import Contact from '../components/Contact/Contact'
 import Projects from '../components/Projects/Projects'
+
 export default class Main extends Component {
     render(){
         return (
@@ -13,6 +15,7 @@ export default class Main extends Component {
                 <Header />
                 
                 <ReactFullpage
+                    licenseKey={LicenseKey}
                     anchors={['welcome', 'about', 'web-dev', 'digital-games', 'analog-games', 'contact']}
                     fitToSectionDelay={800}
                     menu={'#header-menu'}
